1c8c531eba40d00db1454d0cdfc3fff0b87ae1f2
Sacha Greif <git@sachagreif.com>
2023-11-05 20:42:25 +0900

    work

--- index.ts
+++ index.ts
@@ -7,8 +7,9 @@ const wordListPath = require("word-list");
 const wordArray = fs.readFileSync(wordListPath, "utf8").split("\n");
 
 import { words } from "popular-english-words";
+import { logToFile } from "./log_to_file";
 
-const TOP_POPULAR_WORDS = 3000;
+const TOP_POPULAR_WORDS = 2000;
 const MINIMUM_ROOT_LENGTH = 2;
 const MAX_PREFIX_LENGTH = 3;
 
@@ -32,33 +33,14 @@ function getSplitWords(words: string[], prefixLength: number): SplitWord[] {
   );
 }
 
-// function getWordsByPrefix(words: string[], prefixLength: number) {
-//   const splitWords = getSplitWords(words, prefixLength);
-//   const wordsByPrefix = uniq(splitWords.map((w) => w.prefix)).map(
-//     (prefix: string) => {
-//       const roots = splitWords
-//         .filter((w) => w.prefix === prefix)
-//         .map((w) => w.root);
-//       return {
-//         prefix,
-//         roots,
-//         rootCount: roots.length,
-//       };
-//     }
-//   );
-//   // we can discard prefixes that only have a single root attached
-//   const filteredWords = wordsByPrefix.filter((w) => w.rootCount >= 2);
-//   const sortedWordsByPrefix = sortBy(filteredWords, "rootCount").toReversed();
-//   return sortedWordsByPrefix;
-// }
-
 type WordByRoot = {
   root: string;
   prefixes: string[];
   prefixCount: number;
 };
 
-function getWordsByRoot(words: string[]): WordByRoot[] {
+function getAllWordsByRoot(): WordByRoot[] {
+  const words = popularWords;
   let splitWords: SplitWord[] = [];
   // for each prefix length, concatenate all split words in one big array
   for (let i = 1; i <= MAX_PREFIX_LENGTH; i++) {
@@ -85,144 +67,85 @@ function getWordsByRoot(words: string[]): WordByRoot[] {
   return sortedWordsByRoot;
 }
 
-type ValidCombination = {
-  // 4 roots
-  roots: string[];
-  // 9 prefixes
-  prefixes: string[];
-};
-
-// for a given word, keep roots that have 4 or more of the prefixes in common with it
-function filterRoots({
-  wordsByRoot,
-  roots,
-  prefixCount,
-}: {
-  wordsByRoot: WordByRoot[];
-  roots: WordByRoot[];
-  prefixCount: number;
-}) {
-  const disallowedRoots = roots.map((w) => w.root);
-  return (
-    wordsByRoot
-      // exclude the roots that have already been matched
-      .filter((w) => !disallowedRoots.includes(w.root))
-      // store prefixes in common
-      .map((w) => {
-        const commonPrefixes = roots.map((root) =>
-          intersection(w.prefixes, root.prefixes)
-        );
-        return {
-          ...w,
-          commonPrefixes,
-          commonPrefixesCounts: commonPrefixes.map((p) => p.length),
-        };
-      })
-      // only keep items with more than ${prefixCount} prefixes in common for every root
-      .filter((w) => w.commonPrefixesCounts.every((c) => c >= prefixCount))
-  );
-}
-
 type RootCondition = {
   roots: WordByRoot[];
   prefixCount: number;
 };
 
 function applyRootConditions({
-  wordsByRoot,
   previousWords,
   conditions,
 }: {
-  wordsByRoot: WordByRoot[];
   previousWords: WordByRoot[];
   conditions: RootCondition[];
 }) {
-  // { roots: [root1, root2, root3], prefixCount: 1 },
-  // { roots: [root1, root2], prefixCount: 1 },
-  // { roots: [root2, root3], prefixCount: 1 },
-  // { roots: [root1, root3], prefixCount: 1 },
-  // { roots: [root3], prefixCount: 1 },
+  const wordsByRoot = getAllWordsByRoot();
   const disallowedRoots = previousWords.map((w) => w.root);
 
-  //   const c = conditions.map((condition) => {
-  //     const wordWithPrefixesInCommon = getWordsWithPrefixesInCommon(
-  //       wordsByRoot,
-  //       condition.roots
-  //     );
-  //     return { ...condition, wordWithPrefixesInCommon };
-  //   });
-  //   console.log(JSON.stringify(c, null, 2));
   const words = wordsByRoot
     // filter out previously matched roots
     .filter((w) => !disallowedRoots.includes(w.root))
-    .filter((w) => {
-      const m = conditions.every((condition) => {
-        const wordWithPrefixesInCommon = getWordsWithPrefixesInCommon(
-          wordsByRoot,
-          condition.roots
-        );
-        return wordWithPrefixesInCommon.length > condition.prefixCount;
+    .map((w) => {
+      const conditionMatches = conditions.map((condition) => {
+        const allRoots = condition.roots;
+        const prefixesInCommon = intersection.apply(null, [
+          ...allRoots.map((r) => r.prefixes),
+          w.prefixes,
+        ]);
+        const prefixesInCommonCount = prefixesInCommon.length;
+        const meetsCondition = prefixesInCommonCount >= condition.prefixCount;
+        // we remove prefixes to save space when logging out combinations
+        const conditionWithoutPrefixes = {
+          prefixCount: condition.prefixCount,
+          roots: condition.roots.map((r) => {
+            return { root: r.root, prefixCount: r.prefixCount };
+          }),
+        };
+        return {
+          ...conditionWithoutPrefixes,
+          prefixesInCommon,
+          prefixesInCommonCount,
+          meetsCondition,
+        };
       });
-      return m;
-      //   const wordWithPrefixesInCommon = getWordsWithPrefixesInCommon(wordsByRoot, condition.roots);
-      //   console.log(wordWithPrefixesInCommon)
-      //   return { ...w, prefixesInCommon };
+      return { ...w, conditionMatches };
     });
-  console.log(words);
-  return words;
+
+  // only keep words that meet every condition
+  const validWords = words.filter((w) => {
+    return w.conditionMatches.every(
+      (conditionMatch) => conditionMatch.meetsCondition
+    );
+  });
+
+  return validWords;
 }
 
-// for a given list of roots, find all words have prefixes in common with all of them
-function getWordsWithPrefixesInCommon(
-  wordsByRoot: WordByRoot[],
-  roots: WordByRoot[]
-) {
-  const disallowedRoots = roots.map((w) => w.root);
-  return (
-    wordsByRoot
-      // exclude the roots from the search
-      .filter((w) => !disallowedRoots.includes(w.root))
-      .filter((w) =>
-        roots.every((r) => intersection(w.prefixes, r.prefixes).length > 0)
-      )
-  );
-}
+function findRootCombinations() {
+  const wordsByRoot = getAllWordsByRoot();
 
-function findRootCombinations(words: string[]) {
-  const wordsByRoot = getWordsByRoot(words);
   let validCombinations = [];
   for (const root1 of wordsByRoot) {
-    // once we've picked a root, look for any other ones that have 4+ prefixes in common
-    // const validRoots2 = filterRoots({
-    //   wordsByRoot,
-    //   roots: [root1],
-    //   prefixCount: 4,
-    // });
+    // root 2 needs
+    // 4 prefixes in common with root 1
     const validRoots2 = applyRootConditions({
-      wordsByRoot,
       previousWords: [root1],
       conditions: [{ roots: [root1], prefixCount: 4 }],
     });
-    if (validRoots2.length === 0) break;
+    if (validRoots2.length === 0) continue;
 
     // root 3 needs
     // 2 prefixes in common with roots 1/2
     // 1 in common with 2
     for (const root2 of validRoots2) {
       const validRoots3 = applyRootConditions({
-        wordsByRoot,
         previousWords: [root1, root2],
         conditions: [
           { roots: [root1, root2], prefixCount: 2 },
           { roots: [root2], prefixCount: 1 },
         ],
       });
-      //   const validRoots3 = filterRoots({
-      //     wordsByRoot,
-      //     roots: [root1, root2],
-      //     prefixCount: 2,
-      //   });
-      if (validRoots3.length === 0) break;
+      if (validRoots3.length === 0) continue;
 
       // root 4 needs
       // 1 prefix in common with roots 1/2/3
@@ -233,31 +156,25 @@ function findRootCombinations(words: string[]) {
       // 1 in common with 3
       for (const root3 of validRoots3) {
         const validRoots4 = applyRootConditions({
-          wordsByRoot,
           previousWords: [root1, root2, root3],
           conditions: [
             { roots: [root1, root2, root3], prefixCount: 1 },
             { roots: [root1, root2], prefixCount: 1 },
             { roots: [root2, root3], prefixCount: 1 },
             { roots: [root1, root3], prefixCount: 1 },
+            { roots: [root1], prefixCount: 1 },
             { roots: [root3], prefixCount: 1 },
           ],
         });
-        // const validRoots4 = filterRoots({
-        //   wordsByRoot,
-        //   roots: [root1, root2, root3],
-        //   prefixCount: 3,
-        // });
-        if (validRoots4.length === 0) break;
+        if (validRoots4.length === 0) continue;
 
         for (const root4 of validRoots4) {
-          validCombinations.push([root1, root2, root3, root4]);
+          validCombinations.push({ root1, root2, root3, root4 });
         }
       }
     }
   }
-  console.log(validCombinations);
-  console.log(`Found ${validCombinations.length} total valid combinations`);
+  return validCombinations;
 }
 
 function getCombinations(words: string[]) {
@@ -269,11 +186,15 @@ function getCombinations(words: string[]) {
   const threeLetter = getSplitWords(words, 1);
 }
 
-const wordsByRoot = getWordsByRoot(popularWords);
-console.log(wordsByRoot);
-console.log(`${wordsByRoot.length} total words`);
+const wordsByRoot = getAllWordsByRoot();
+// console.log(wordsByRoot);
+// console.log(`${wordsByRoot.length} total words`);
 
-findRootCombinations(popularWords);
+const validCombinations = findRootCombinations();
+
+console.log(`Found ${validCombinations.length} total valid combinations`);
+logToFile("wordsByRoots.yml", getAllWordsByRoot());
+logToFile("validCombinations.json", validCombinations);
 
 // const combos3 = getWordsByPrefix(popularWords, 3);
 // console.log(combos3);
---
+++ log_to_file.ts
@@ -0,0 +1,78 @@
+import fs from "fs";
+import yaml from "js-yaml";
+// @ts-ignore
+import fsp from "fs/promises";
+
+export type LogOptions = {
+  mode?: "append" | "overwrite";
+  timestamp?: boolean;
+  dirPath?: string;
+  subDir?: string;
+};
+
+async function exists(path: string) {
+  try {
+    await fsp.access(path);
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+const getExtension = (fileName: string) => fileName.split(".").at(-1) || "none";
+
+export const logToFile = async (
+  fileName_: string,
+  object: any,
+  options: LogOptions = {}
+) => {
+  if (process.env.NODE_ENV === "development") {
+    let fileName = fileName_,
+      subDir = options?.subDir;
+    const extension = getExtension(fileName);
+    const { mode = "overwrite", timestamp = false, dirPath } = options;
+    const envLogsDirPath = "/Users/sacha/dev/standstill/standstill-bun/.logs";
+
+    if (!envLogsDirPath) {
+      console.warn(
+        "Please define LOGS_PATH in your .env file to enable logging"
+      );
+      return;
+    }
+
+    if (fileName.includes("/")) {
+      subDir = fileName.split("/")[0];
+      fileName = fileName.split("/")[1];
+    }
+
+    const logsDirPath =
+      dirPath ?? (subDir ? `${envLogsDirPath}/${subDir}` : envLogsDirPath);
+
+    if (!(await fs.existsSync(logsDirPath))) {
+      fs.mkdirSync(logsDirPath, { recursive: true });
+    }
+    const fullPath = `${logsDirPath}/${fileName}`;
+    let contents;
+    if (typeof object === "string") {
+      contents = object;
+    } else {
+      if (["yml", "yaml"].includes(extension)) {
+        contents = yaml.dump(object, { noRefs: true, skipInvalid: true });
+      } else {
+        contents = JSON.stringify(object, null, 2);
+      }
+    }
+    if (!contents) {
+      return;
+    }
+    const now = new Date();
+    const text = timestamp ? now.toString() + "\n---\n" + contents : contents;
+    if (mode === "append") {
+      const stream = fs.createWriteStream(fullPath, { flags: "a" });
+      stream.write(text + "\n");
+      stream.end();
+    } else {
+      await fsp.writeFile(fullPath, text);
+    }
+  }
+};
